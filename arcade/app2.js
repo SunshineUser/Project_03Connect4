7eaba88ee6d81de402323a75d7bdc1f19abbd3a4
user_sunshine <ian.n.sorensen@gmail.com>
2022-10-08 18:07:10 -0600

    functioning game with incomplete post game settings

--- arcade/app2.js
+++ arcade/app2.js
@@ -31,7 +31,6 @@ let gameState= {
         }
     }
 
-
 }
 
 //Dom loader wooo > this event occurs when your html document gets rendered on the DOM
@@ -57,12 +56,12 @@ function renderGameBoard(){
         //create cells for each row 
         for (let numOfCellsMade = 0; numOfCellsMade< currentJSRow.length; numOfCellsMade++){
             let newCellElement = document.createElement("section")
-            newCellElement.classList.add(`${[numOfRowsMade,numOfCellsMade]}`)
+            newCellElement.id=(`${[numOfRowsMade,numOfCellsMade]}`)
             newCellElement.classList.add("cell");
-           
-
-            
-            
+            let imgr=document.createElement("img");
+            imgr.classList.add("click");
+            imgr.src = "pokeball.png";
+            imgr.id= `img${numOfRowsMade},${numOfCellsMade}`;
 
             if(currentJSRow[numOfCellsMade] != 0){
                 newCellElement.textContent = currentJSRow[numOfCellsMade];
@@ -70,6 +69,7 @@ function renderGameBoard(){
             
                 newCellElement.textContent = "";
             }
+            newCellElement.appendChild(imgr);
             newRowElement.appendChild(newCellElement);
             
         }
@@ -89,21 +89,34 @@ document.addEventListener("DOMContentLoaded",renderGameBoard);
 //place a piece on the grid based on location clicked (aka classes are dumb)
 function makeYourMove(event)
 {
-    let clickLocationColumn = event.target.classList.value.charAt(2);
-    clickLocationColumn = parseInt(clickLocationColumn);
-    console.log ("clickLocCol" + clickLocationColumn)
+    //console.dir(event.target);
+    let clickLocationColumn = event.target.id.charAt(5);
+    //console.log(clickLocationColumn);
+    //turns it into an Int
+    clickLocationColumn = clickLocationColumn*1;
+    //console.log ("clickLocCol" + clickLocationColumn)
     //what column the person clicked
-    console.log(gameState.gameGrid[0][clickLocationColumn]);
+    //console.log(gameState.gameGrid[0][clickLocationColumn]);
     for(let rowLocation=5; rowLocation >=0 ; rowLocation-- )
     {
     //if the bottom array location == 0 make it == 1 
-        
-        if(0 == gameState.gameGrid[rowLocation][clickLocationColumn])
+    
+        // check if current array value is 0 && contains the class element appended to the cell image (click) to prevent margin clicking errors
+        if( gameState.gameGrid[rowLocation][clickLocationColumn] ==0  && event.target.classList.contains("click"))
         {
+            console.log("make your movefunction"+ rowLocation+clickLocationColumn)
+
+            //updates current player token location
             gameState.gameGrid[rowLocation][clickLocationColumn] = gameState.currentPlayerToken[0];
             tokenVisualEleUpdate(rowLocation,clickLocationColumn);
             gameState.swapCurrentPlayerToken();
+            //check victory function and exit game run exit game function if true
+            if(checkVictoryAll(gameState.gameGrid)== true){
+                
+            };
+
             return console.log(gameState.gameGrid);    
+
         }
         else
         {
@@ -118,24 +131,160 @@ function makeYourMove(event)
 
 function tokenVisualEleUpdate(row,column){
     if (gameState.gameGrid[row][column]=='r')
-    {
+    {   
         console.log(gameState.gameGrid[row][column]);
-        let currentEle = document.getElementsByClassName(`${row},${column}`)[0]
-        console.log(currentEle);
+
         
-        let imgr=document.createElement("img");
+        let imgr = document.getElementById(`img${row},${column}`)
         imgr.src="charmander.jpg";
 
-        currentEle.appendChild(imgr);
     }else if (gameState.gameGrid[row][column]=='b')
+
     {
         console.log(gameState.gameGrid[row][column]);
-        let currentEle = document.getElementsByClassName(`${row},${column}`)[0]
-        console.log(currentEle);
-        
-        let imgr=document.createElement("img");
-        imgr.src="squirtle.jpg";
 
-        currentEle.appendChild(imgr);
+        
+        let imgr = document.getElementById(`img${row},${column}`)
+        imgr.src="squirtle.jpg";
 }
+}
+
+
+
+
+// I wish we got into using multiple JS pages because this solution checker is long and going to be tedious to implement and I wrote it in a different .js file because I knew that fact
+function winSearchUpDown(grid){
+    for(let column= 0; column <=6; column ++)
+    {
+        let winChecker = 1;
+        let currentLocationValue = 'z';
+        
+        //iterate each cell within the rows
+        for(let row = 0; row<=grid.length-1; row++){
+            //create a value that iterates every time it shares a value with another cell, and replaces itself every time it doesn't
+            //console.log(grid[row][column]);
+
+            //full grid search for left right victory 
+            if(currentLocationValue == grid[row][column] && grid[row][column]!= 0){
+                winChecker = winChecker +1;
+                //console.log("winCheckerValue"+ winChecker +" row"+ row + "column" + column);
+                if (victory(winChecker) == true){
+                    return true
+                }
+
+            }else {
+                //console.log(grid[row][column]);
+                currentLocationValue = grid[row][column];
+                //console.log("currentLocationValue" + row + column  + currentLocationValue)
+                winChecker = 1;
+            }
+        }
+    }
+}
+
+
+function winSearchRightLeft(grid){
+
+    //iterate each row
+    for(let row= 0; row<=5; row ++)
+    {
+        let winChecker = 1;
+        let currentLocationValue = 'z';
+        
+        //iterate each cell within the rows
+        for(let cell = 0; cell<grid[0].length-1; cell++){
+            //create a value that iterates every time it shares a value with another cell, and replaces itself every time it doesn't
+            grid[row][cell];
+
+            //full grid search for left right victory 
+            if(currentLocationValue == grid[row][cell] && grid[row][cell]!= 0){
+                winChecker = winChecker +1;
+                //console.log("winCheckerValue"+ winChecker +" row"+ row + "column" + cell);
+                if (victory(winChecker) == true){
+                    return true
+                }
+
+            }else {
+                //console.log(grid[row][cell]);
+                currentLocationValue = grid[row][cell];
+                //console.log("currentLocationValue" + row +cell  + currentLocationValue)
+                winChecker = 1;
+            }
+        }
+    }
+}
+
+function winSearchDiagonalUpR(grid){
+    
+    //index and iterate top right of grid from 0,3-0,6 to 3,3- 3,6 (because our win conditions for diagonals can only start from this range we can search in a smaller iterated grid)
+    for(let row = 0; row< 3; row ++){
+        //column iteration for smaller grid
+        for(let cell =3; cell<= 5; cell++){
+            //for loop cycling with i for 3 iterations max to determine if there's 4 in a row
+            for(let inARow=1; inARow>0; inARow++){
+                //if there's a matching non-zero value at +1, -1 inARow = 2, if there's another non matching value at +2-2, inARow = 3 "" until inARow ==4 else, break
+                //console.log("row " + row + "cell " + cell);
+                console.log(grid[row][cell]+ " " + grid[row+inARow][cell-inARow]);
+                if(grid[row][cell] == grid[row+inARow][cell-inARow] && grid[row][cell]!= 0){
+                    //iteratelmao
+                    //console.log(inARow);
+                    // gross solution to an offbyone Error
+                    if(victory (inARow+1) ==true){
+                        return true
+                    };
+                }
+                else {
+                    //not matching return
+
+                    //console.log(inARow);
+                    inARow= -1;
+                }
+            
+            }
+        
+        }
+    }
+}
+
+function winSearchDiagonalUpL(grid){
+    
+    //index and iterate top right of grid from 0,3-0,6 to 3,3- 3,6 (because our win conditions for diagonals can only start from this range we can search in a smaller iterated grid)
+    for(let row = 0; row<= 3; row ++){
+        //column iteration for smaller grid
+        for(let cell =0; cell<= 3; cell++){
+            //for loop cycling with i for 3 iterations max to determine if there's 4 in a row
+            for(let inARow=1; inARow>0; inARow++){
+                //if there's a matching non-zero value at +1, -1 inARow = 2, if there's another non matching value at +2-2, inARow = 3 "" until inARow ==4 else, break
+                //console.log("row " + row + "cell " + cell);
+                if(grid[row][cell] == grid[row+inARow][cell+inARow] && grid[row][cell]!= 0){
+                    //iteratelmao
+                    //console.log(inARow);
+                    // gross solution to an offbyone Error
+                    if(victory (inARow+1) ==true){
+                        return true 
+                    };
+                }
+                else {
+                    //not matching return
+
+                    //console.log(inARow);
+                    inARow= -1;
+                }
+            
+            }
+        
+        }
+    }
+}
+
+function checkVictoryAll(grid)
+{
+    return (winSearchDiagonalUpR(grid) == true || winSearchRightLeft(grid) == true || winSearchDiagonalUpL(grid) == true ||winSearchUpDown || true)
+}
+
+function victory(index){
+    if(index >= 4){
+        console.log("You've won!!!");
+        return true
+    }
 }
